59100833612834ca00bfaa75c9cedd4e69762eb5
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-04-15 10:08:02 +1000

    added Em and Sm

--- Uaine.Math/Constants/Sci/Physics.cs
+++ Uaine.Math/Constants/Sci/Physics.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Uaine.Math.Constants.Sci
+﻿namespace Uaine.Math.Constants.Sci
 {
     public static class Physics
     {
+        //Astro-masses in KG
+        public static double EarthMass = 5.972E+24;
+        public static double SolarMass = 1.98847E+30;
     }
 }
