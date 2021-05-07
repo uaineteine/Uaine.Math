78cec7f9590c0623005ed4a56e6ed40fc52803f3
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-05-07 12:01:22 +1000

    generic interpolation

--- Uaine.Math/Interpolation/Interpolator.cs
+++ Uaine.Math/Interpolation/Interpolator.cs
@@ -18,5 +18,11 @@ namespace Uaine.Math.Interpolation
             eq.SolveLeastSquares(new float[] { 0, 1 }, yvals);
             return eq.f(xi);
         }
+
+        public float Interpolation(float x, float[] xi, float[] yi)
+        {
+            eq.SolveLeastSquares(xi, yi);
+            return eq.f(x);
+        }
     }
 }
