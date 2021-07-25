fd6bf166a3c166682a396f4f0eaf24c3f5b5bb9c
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-07-25 18:27:15 +1000

    added more constants in astro and physics

--- Uaine.Math/Constants/Sci/Physics.cs
+++ Uaine.Math/Constants/Sci/Physics.cs
@@ -5,5 +5,12 @@
         //Astro-masses in KG
         public static double EarthMass = 5.972E+24;
         public static double SolarMass = 1.98847E+30;
+        public static double JupiterMass = 1.899E+27;
+        //ASTRO RADII in M
+        public static double SolarRadius = 6.9634E+8;
+        //IN SI
+        public static double AtomicMassUnit = 1.66054E-27;
+        public static double StefanBoltzmanConstant = 5.67051E-8;
+        public static double BoltzmannConstant = 1.899E+30;
     }
 }
