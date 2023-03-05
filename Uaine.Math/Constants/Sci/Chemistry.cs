7cba81943df18af03507e90eb6cb2d32d4ac7f16
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2023-03-05 20:23:44 +1100

    Updated dependency math package Added a few chemistry constants

---
+++ Uaine.Math/Constants/Sci/Chemistry.cs
@@ -0,0 +1,8 @@
+﻿namespace Uaine.Math.Constants.Sci
+{
+    public static class Chemistry
+    {
+        public static double Avogadronstant = 6.0221408E23;
+        public static double ElectronCharge = 1.60217663E-19; // in C
+    }
+}
